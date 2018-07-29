fc1d2af789f60e5f33c302893a17825b26ad5920
Tallerik <erik.klasse@gmail.com>
2018-07-29 16:41:34 +0200

    Update Python Backend - Added Login - Added Logout

--- Backend/PythonWebServer.py
+++ Backend/PythonWebServer.py
@@ -5,6 +5,40 @@ from flask import Response
 
 # import Package 'Json'
 import json
+# import Package mysql.connector for Database connection
+import mysql.connector
+# import Libs for hashing
+import bcrypt
+
+import string
+import random
+
+
+def getRandomSessionID():
+    min_char = 8
+    max_char = 12
+    allchar = string.ascii_letters
+    password = "".join(random.choice(allchar) for x in range(random.randint(min_char, max_char)))
+    return password
+
+
+#Password Hashing
+def get_hashed_password(plain_text_password):
+    # Hash a password for the first time
+    plain_text_password = plain_text_password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    return [bcrypt.hashpw(plain_text_password, salt), salt]
+
+def check_password(plain_text_password, hashed_password):
+    # Check hased password. Useing bcrypt, the salt is saved into the hash itself
+    plain_text_password = plain_text_password.encode("utf-8")
+    hashed_password = hashed_password.encode("utf-8")
+    return bcrypt.checkpw(plain_text_password, hashed_password)
+
+# Gets Database login
+raw = open("database.json")
+raw = raw.read()
+sql = json.loads(raw)
 
 #Variable app als Flask initialisieren
 app = Flask(__name__)
@@ -23,7 +57,107 @@ def main():
     # Gibt den Generierten Json zurück, sodass er ausgegeben wird (Hier mit dem Argument das der mimetype="application/json" ist).
     return Response(out, mimetype="application/json")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    cnx = mysql.connector.connect(user=sql['user'], password=sql['pw'],
+                              host=sql['host'],
+                              database=sql['db'])
+    cursor = cnx.cursor()
+
+    user = request.args.get('user')
+    pwRaw = request.args.get('pw')
+    temp = get_hashed_password(pwRaw)
+    hashed = temp[0]
+    salt = temp[1]
+    query = ("SELECT * From User WHERE Name='") + str(user) + ("'")
+    #queryData = (user)
+
+
+    if not cursor.execute(query):
+        for (Name, pw, salt) in cursor:
+            if check_password(pwRaw, pw):
+                query = ("INSERT INTO Sessions (SessionID, Name) VALUES (%s, %s)")
+                session = getRandomSessionID()
+                queryData = (session, user)
+                cursor.execute(query, queryData)
+                out = json.dumps({
+                    "error": False,
+                    "name": Name,
+                    "SessionID": session
+                })
+            else:
+                out = json.dumps({
+                    "error": True,
+                    "stacktrace": {
+                        "type": "Account",
+                        "stacktrace": "Wrong Login Infos!"
+                    }
+                })
+    else:
+        out = json.dumps({
+        "error": True,
+        "stacktrace": {
+            "type": "SQL",
+            "stacktrace": "Error in SQL Connection"
+        }
+    })
+
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+    return Response(out, mimetype="application/json")
+
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    sessionID = request.args.get('session')
+    cnx = mysql.connector.connect(user=sql['user'], password=sql['pw'],
+                              host=sql['host'],
+                              database=sql['db'])
+    cursor = cnx.cursor()
+    query = ("DELETE From Sessions WHERE SessionID='") + str(sessionID) + ("'")
+
+    try:
+        cursor.execute(query)
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+        out = json.dumps({
+            "error": False,
+            "result": "Log out complete"
+        })
+    except mysql.connector.Error:
+        out = json.dumps({
+            "error": True,
+            "stacktrace": {
+                "type": "Session",
+                "stacktrace": "Session is invalid"
+            }
+        })
+
+
+    return Response(out, mimetype="application/json")
+
+
+    if not cursor.execute(query) == None:
+        out = json.dumps({
+            "error": False,
+            "result": "Log out complete"
+        })
+    else:
+        out = json.dumps({
+            "error": True,
+            "stacktrace": {
+                "type": "Session",
+                "stacktrace": "Session is invalid"
+            }
+        })
+    
+
 # Start Object
 if __name__ == "__main__":
     # Starts the Webserver
-    app.run()
+    app.run(debug=True)
